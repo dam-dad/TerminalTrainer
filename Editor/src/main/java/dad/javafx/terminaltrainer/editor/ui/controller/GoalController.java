c6752197b1233d628d8414efd816c0c664bcf3d2
Tevu-W <55486791+Tevu-W@users.noreply.github.com>
2021-01-28 16:35:05 +0000

    Goal View and Goal Controller

---
+++ Editor/src/main/java/dad/javafx/terminaltrainer/editor/ui/controller/GoalController.java
@@ -0,0 +1,78 @@
+package dad.javafx.terminaltrainer.editor.ui.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+
+public class GoalController implements Initializable {
+
+	
+	@FXML
+    private GridPane view;
+
+    @FXML
+    private TextField textDescription;
+
+    @FXML
+    private ComboBox<?> comboShell;
+
+    @FXML
+    private TextField textPWD;
+
+    @FXML
+    private TextField textUser;
+
+    @FXML
+    private ListView<?> ListCommands;
+
+    @FXML
+    private ListView<?> ListTips;
+
+    @FXML
+    private Button btnAddCommand;
+
+    @FXML
+    private Button btnRemoveCommand;
+
+    @FXML
+    private Button btnAddTip;
+
+    @FXML
+    private Button btnRemoveTip;
+
+    @FXML
+    void onAddCommandAction(ActionEvent event) {
+    	
+    }
+
+    @FXML
+    void onAddTipAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onRemoveCommandAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onRemoveTipAction(ActionEvent event) {
+    	
+    }
+    	
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
