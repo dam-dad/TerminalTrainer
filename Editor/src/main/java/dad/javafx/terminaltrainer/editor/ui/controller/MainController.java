bb47d67b1f6724943900f04c6c5631e20a2194f9
Aitor <55486830+Splasy@users.noreply.github.com>
2021-01-30 17:51:20 +0000

    Función de guardado añadida

--- Editor/src/main/java/dad/javafx/terminaltrainer/editor/ui/controller/MainController.java
+++ Editor/src/main/java/dad/javafx/terminaltrainer/editor/ui/controller/MainController.java
@@ -1,10 +1,14 @@
 package dad.javafx.terminaltrainer.editor.ui.controller;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import dad.javafx.terminaltrainer.editor.model.Challenge;
+import dad.javafx.terminaltrainer.editor.model.Goal;
+import dad.javafx.terminaltrainer.editor.ui.app.App;
+import dad.javafx.terminaltrainer.utils.JSONUtils;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.value.ObservableValue;
@@ -14,14 +18,17 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.SplitPane;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
 
 public class MainController implements Initializable {
 	// controladores
 	ChallengeController challengeController = new ChallengeController();
 	GoalController goalController = new GoalController();
-	
-	//Model
+
+	// Model
 	private ObjectProperty<Challenge> challenge = new SimpleObjectProperty<>();
+	private ObjectProperty<Goal> goal = new SimpleObjectProperty<>();
 
 	@FXML
 	private BorderPane view;
@@ -39,26 +46,47 @@ public class MainController implements Initializable {
 	public void initialize(URL location, ResourceBundle resources) {
 		split.setDividerPositions(0.5, 0.5);
 		split.getItems().addAll(challengeController.getView(), goalController.getView());
-		
+
 		challenge.addListener((o, ov, nv) -> onChallengeChanged(o, ov, nv));
+
+		challenge.set(new Challenge());
 
 	}
 
 	private void onChallengeChanged(ObservableValue<? extends Challenge> o, Challenge ov, Challenge nv) {
-		if(ov != null) {
+		if (ov != null) {
 			challengeController.challengeProperty().unbind();
-			//TODO desbindear el resto de elementos
+			goalController.goalProperty().unbind();
+			// TODO desbindear el resto de elementos
 		}
-		
-		if(nv != null) {
+
+		if (nv != null) {
 			challengeController.challengeProperty().bind(challenge);
-			//TODO bindear el resto de elementos
+			goalController.goalProperty().bind(goal);
+			// TODO bindear el resto de elementos
 		}
 	}
 
 	@FXML
-	void onNuevoAction(ActionEvent event) {//Sólo desbindea 1 elemento de la tabla, mirar desbindeos.
+	void onNewAction(ActionEvent event) {// No desbindea la tabla.
 		challenge.set(new Challenge());
+	}
+
+	@FXML
+	void onSaveAction(ActionEvent event) {
+		FileChooser fileChooser = new FileChooser();
+		fileChooser.setTitle("Guardar un challenge.");
+		fileChooser.getExtensionFilters().add(new ExtensionFilter("Challenge (*.ch)", "*.ch"));
+		fileChooser.getExtensionFilters().add(new ExtensionFilter("Todos los archivos", "*.*"));
+		File cvFile = fileChooser.showSaveDialog(App.getPrimaryStage());
+		if (cvFile != null) {
+			try {
+				JSONUtils.toJson(cvFile, challenge.get());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+		}
 	}
 
 	public BorderPane getView() {
